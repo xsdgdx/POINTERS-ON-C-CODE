76b79f2e38d3f5ae889bb0552002f7bd657a81f7
xsdgdx <894501246@qq.com>
2019-03-11 19:06:08 +0800

    frist test

--- programs/ch2/test2.cpp
+++ programs/ch2/test2.cpp
@@ -2,9 +2,7 @@
 #include<stdio.h>
 int main()
 {
-double d;
-char str[] = "123.456";
-d=atof(str);
-printf("string=%s double=%lf\n",str,d);
-return 0;
+    printf("hello world");
+    return 0;
 }
+
